9ef3d9addd63d5a0b088338f042adde82b204865
Jose Lorenzo <jolorenzom@gmail.com>
2018-09-14 20:57:02 +0200

    Importados paquetes para testear

--- src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
+++ src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
@@ -1,4 +1,10 @@
 package es.upm.miw.iwvg.forge.junit;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
+
 }
