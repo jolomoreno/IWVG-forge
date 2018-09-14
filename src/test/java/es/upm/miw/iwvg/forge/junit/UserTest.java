2813b35d2ce20c81e237dd16e56e3ae47a02a34e
Jose Lorenzo <jolorenzom@gmail.com>
2018-09-14 21:01:28 +0200

    Comenzar a testear User

--- src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
+++ src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
@@ -6,5 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
+    private User user;
 
+    @BeforeEach
+    void before() {
+        user = new User(1, "", "");
+    }
+
+    @Test
+    void testUser() {
+    }
 }
